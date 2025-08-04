d9edb97b1c1b4bcee0cd0f8536d57dde7786d95d
Martijn <martijn@competa.com>
2025-08-04 14:47:03 +0200

    remove console log

--- src/components/atoms/CheckBox.tsx
+++ src/components/atoms/CheckBox.tsx
@@ -5,7 +5,6 @@ interface Props {
 }
 
 const CheckBox = ({ isCompleted, onChange, name }: Props) => {
-  console.log('ee')
   return (
     <label className="flex items-center cursor-pointer">
       <h1>H1 example</h1>
