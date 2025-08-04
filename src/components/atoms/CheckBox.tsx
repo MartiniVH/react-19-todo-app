ec41ea98fdf9c8d64781d1e553b1cf4210970cc9
Martijn <martijn@competa.com>
2025-08-04 17:10:50 +0200

    add test for todo app

--- src/components/atoms/CheckBox.tsx
+++ src/components/atoms/CheckBox.tsx
@@ -5,6 +5,7 @@ interface Props {
 }
 
 const CheckBox = ({ isCompleted, onChange, name }: Props) => {
+  console.log('test for todo app')
   return (
     <label className="flex items-center cursor-pointer">
       <h1>H1 example</h1>
